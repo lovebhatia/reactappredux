924508fc2d0b968e72753a2f69a25eae64e3eaef
lovebhatia <javalove007@gmail.com>
2020-09-06 01:32:18 +0530

    3 files modified

--- redux/index.js
+++ redux/index.js
@@ -1,44 +1,28 @@
 import redux, {createStore} from "redux"
-/**
- * Challenge: set up redux action creators, reducer, and store
- * We'll be building a counter app to start out.
- * Read the comments below for the step-by-step challenges
- */
-
-// 1. Create action creators for having the count "increment" and "decrement"
 
 function increment() {
-  return {
-    type : "INCREMENT"
-  }
+    return {
+        type: "INCREMENT"
+    }
 }
 
 function decrement() {
-  return {
-    type : "Decrement"
-  }
+    return {
+        type: "DECREMENT"
+    }
 }
 
-// 2. Create a reducer to handle your increment and decrement actions
-function countReducer(count = 0,action) {
-  switch(action.type) {
-    case "INCREMENT" :
-      return count + 1
-    case "INCREMENT" :
-      return count -1
-      default:
-       return count
-
-  }
+function reducer(count = 0, action) {
+    switch(action.type) {
+        case "INCREMENT":
+            return count + 1
+        case "DECREMENT":
+            return count - 1
+        default:
+            return count
+    }
 }
 
-
-// 3. Create a new Redux store
-const store=createStore(countReducer)
-
-// 4. Set up the subscribe function so we can more easily see the changes to the Redux state as they happen
+const store = createStore(reducer)
 store.subscribe(() => console.log(store.getState()))
-
-
-// 5. Export the store as a default export
 export default store
--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import {connect} from "react-redux"
 import "./style.css";
 
-export default function App() {
+ function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
@@ -9,3 +10,18 @@ export default function App() {
     </div>
   );
 }
+
+function mapStateToProps(globalState) {
+    // return an object where the keys are the name of the prop your component wants,
+    // values are the actual parts of the global state your component wants
+}
+
+
+
+export default connect(mapStateToProps,/*what actions to dispatch */)(App)
+
+//connect is a function and it returns a function to which we pass our component
+//export default connect(/* what parts of state you want*/ /*,*//*what actions to dispatch */)(App)
+
+//functions that can return functions and calling them immediately.
+
