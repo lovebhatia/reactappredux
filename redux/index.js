ec0d03d1102ce5d8b7a432ca0ccdbc83aeb968ef
lovebhatia <javalove007@gmail.com>
2020-09-05 23:38:40 +0530

    1 file modified, 1 added

---
+++ redux/index.js
@@ -0,0 +1,44 @@
+import redux, {createStore} from "redux"
+/**
+ * Challenge: set up redux action creators, reducer, and store
+ * We'll be building a counter app to start out.
+ * Read the comments below for the step-by-step challenges
+ */
+
+// 1. Create action creators for having the count "increment" and "decrement"
+
+function increment() {
+  return {
+    type : "INCREMENT"
+  }
+}
+
+function decrement() {
+  return {
+    type : "Decrement"
+  }
+}
+
+// 2. Create a reducer to handle your increment and decrement actions
+function countReducer(count = 0,action) {
+  switch(action.type) {
+    case "INCREMENT" :
+      return count + 1
+    case "INCREMENT" :
+      return count -1
+      default:
+       return count
+
+  }
+}
+
+
+// 3. Create a new Redux store
+const store=createStore(countReducer)
+
+// 4. Set up the subscribe function so we can more easily see the changes to the Redux state as they happen
+store.subscribe(() => console.log(store.getState()))
+
+
+// 5. Export the store as a default export
+export default store
