3ef9553f4012a636edcffe80f70123f4ddc609a5
lovebhatia <javalove007@gmail.com>
2020-09-06 15:05:05 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React from "react";
 import {connect} from "react-redux"
 import "./style.css";
 
- function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+function App(props) {    
+    return (
+        <div>
+            <h1>{props.count} </h1>
+            <button>-</button>
+            <button>+</button>
+        </div>
+    )
 }
-
 function mapStateToProps(globalState) {
     // return an object where the keys are the name of the prop your component wants,
+  return {
+    //bananas: "Hello"
+   // bananas: globalState
+   count: globalState
+  }
+
+
+
     // values are the actual parts of the global state your component wants
 }
 
