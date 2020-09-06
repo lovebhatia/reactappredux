3f9a537f2c090959d86d7f0a40217f827045d2b5
lovebhatia <javalove007@gmail.com>
2020-09-06 15:37:45 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import {connect} from "react-redux"
 import "./style.css";
+import {increment,decrement} from "../redux"
+
 
 function App(props) {    
     return (
         <div>
             <h1>{props.count} </h1>
-            <button>-</button>
-            <button>+</button>
+            <button onClick={props.increment}>-</button>
+            <button onClick={props.decrement}>+</button>
         </div>
     )
 }
+/*
 function mapStateToProps(globalState) {
     // return an object where the keys are the name of the prop your component wants,
   return {
@@ -23,10 +26,21 @@ function mapStateToProps(globalState) {
 
     // values are the actual parts of the global state your component wants
 }
+*/
+/*
+const mapDispatchToProps = {
+
+  increment:increment,
+  decrement: decrement 
+
+}
+*/
+// https://react-redux.js.org/api/connect#connect
 
 
+//export default connect(mapStateToProps,mapDispatchToProps)(App)
+export default connect(state => ({count: state}),{increment,decrement})(App)
 
-export default connect(mapStateToProps,/*what actions to dispatch */)(App)
 
 //connect is a function and it returns a function to which we pass our component
 //export default connect(/* what parts of state you want*/ /*,*//*what actions to dispatch */)(App)
